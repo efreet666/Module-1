633119efd7d2af6fc29dab6a8891163063a02ff3
Vlad <vladislav.bok@yandex.ru>
2022-08-02 17:01:42 +0300

    Initial Commit

---
+++ Module 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module 1
+//
+//  Created by Влад Бокин on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
